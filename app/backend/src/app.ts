51304f1161be681b7b5ae39c4c7284deb154c9fa
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-05-30 22:28:35 -0300

    build(app)
    implementado as alterações para funcionamento do app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,12 +2,10 @@ import * as express from 'express';
 
 class App {
   public app: express.Express;
-  // ...
 
   constructor() {
-    // ...
+    this.app = express();
     this.config();
-    // ...
   }
 
   private config():void {
@@ -19,12 +17,10 @@ class App {
     };
 
     this.app.use(accessControl);
-    // ...
   }
 
-  // ...
   public start(PORT: string | number):void {
-    // ...
+    this.app.listen(PORT, () => console.warn('Listen on', PORT));
   }
 }
 
