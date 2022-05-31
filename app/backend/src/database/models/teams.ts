bd4398d3fd67d7a2fd613975ecaf5d082a6d939c
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-05-30 22:23:11 -0300

    feat(model teams)
    Implementado a model de teams

---
+++ app/backend/src/database/models/teams.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id!: number;
+  public teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Team;
