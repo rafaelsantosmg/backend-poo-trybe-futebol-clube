02a97567520c65cb78f84ff2fb4a025efe841048
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-01 19:43:37 -0300

    refactor(Schemas)
    implementado a mensagem de erro correta para validação de email do Joi

--- app/backend/src/schemas/joiSchemas.ts
+++ app/backend/src/schemas/joiSchemas.ts
@@ -1,17 +1,23 @@
 import Joi = require('joi');
 
+const ANY_REQUIRED = 'All fields must be filled';
+
 export default class JoiSchemas {
   user = Joi.object().keys({
     username: Joi.string().min(8),
     role: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(6).required().messages({
-      'string.min': '"password" length must be 6 characters long',
-    }),
+    password: Joi.string().min(6).required(),
   });
 
   login = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    email: Joi.string().required().messages({
+      'any.required': ANY_REQUIRED,
+      'string.empty': ANY_REQUIRED,
+    }),
+    password: Joi.string().required().messages({
+      'any.required': ANY_REQUIRED,
+      'string.empty': ANY_REQUIRED,
+    }),
   });
 }
