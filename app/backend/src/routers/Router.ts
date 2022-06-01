7b3b45c984a9f2c000fe7d18719d7af286a9a80e
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-01 19:41:40 -0300

    feat(Router)
    Implementando a routa da login/validate

--- app/backend/src/routers/Router.ts
+++ app/backend/src/routers/Router.ts
@@ -1,17 +1,23 @@
 import { Application } from 'express';
 import JoiSchemas from '../schemas/joiSchemas';
 import LoginControler from '../controllers/Login';
-import ValidateJoi from '../middlewares/validateJoi';
+import Validations from '../middlewares/validations';
 
 export default class Routers {
   private _loginControler = new LoginControler();
-  private _validateJoi = new ValidateJoi();
+  private _validations = new Validations();
   private _joiSchemas = new JoiSchemas();
 
   public login(app: Application): void {
+    app.get(
+      '/login/validate',
+      (req, res, next) => this._validations.auth(req, res, next),
+      (req, res) => this._loginControler.loginValidate(req, res),
+    );
+
     app.post(
       '/login',
-      (req, res, next) => this._validateJoi.joi(req, res, next, this._joiSchemas.login),
+      (req, res, next) => this._validations.joi(req, res, next, this._joiSchemas.login),
       (req, res) => this._loginControler.login(req, res),
     );
   }
