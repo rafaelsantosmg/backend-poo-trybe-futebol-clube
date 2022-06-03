bcb830d4e522b273007f5fba7940d840e31d6843
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-03 20:56:57 -0300

    feat(APP)
    Adcionado um arquivo de tipagems para a aplicação

---
+++ app/backend/src/types/TUser.ts
@@ -0,0 +1,15 @@
+export type TUserValidate = {
+  user: {
+    id: number;
+    username: string;
+    role: string;
+    email: number;
+  },
+};
+
+export type TUserModel = {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+};
