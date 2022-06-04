09a46f86e2182414e793cd37ba15f71dabd8e2e7
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-04 00:34:31 -0300

    refactor(Controller)
    retirado os try cath das rotas de get

--- app/backend/src/controllers/Matches.ts
+++ app/backend/src/controllers/Matches.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import ThrowError from '../utils/throwError';
 import MatcheService from '../services/Matches';
 
 export default class MatchesController {
@@ -9,13 +8,7 @@ export default class MatchesController {
   async getAll(req: Request, res: Response) {
     const { inProgress } = req.query;
     this._inProgress = inProgress === 'true';
-    try {
-      const matches = await this._matcheService.getAll(this._inProgress);
-      return res.status(200).json(matches);
-    } catch (error) {
-      console.log(error);
-      const { status, message } = error as ThrowError;
-      return res.status(status).json({ message });
-    }
+    const matches = await this._matcheService.getAll(this._inProgress);
+    return res.status(200).json(matches);
   }
 }
--- app/backend/src/controllers/Teams.ts
+++ app/backend/src/controllers/Teams.ts
@@ -1,28 +1,17 @@
 import { Request, Response } from 'express';
-import ThrowError from '../utils/throwError';
 import TeamService from '../services/Teams';
 
 export default class teamsController {
   private _teamsService = new TeamService();
 
   async getAll(req: Request, res: Response) {
-    try {
-      const teams = await this._teamsService.getAll();
-      return res.status(200).json(teams);
-    } catch (error) {
-      const { status, message } = error as ThrowError;
-      return res.status(status).json({ message });
-    }
+    const teams = await this._teamsService.getAll();
+    return res.status(200).json(teams);
   }
 
   async getById(req: Request, res: Response) {
     const { id } = req.params;
-    try {
-      const teams = await this._teamsService.getById(id);
-      return res.status(200).json(teams);
-    } catch (error) {
-      const { status, message } = error as ThrowError;
-      return res.status(status).json({ message });
-    }
+    const teams = await this._teamsService.getById(id);
+    return res.status(200).json(teams);
   }
 }
