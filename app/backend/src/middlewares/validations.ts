c9a9bee83ede49c5c740ccf8fd8a35781b92131f
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-05 18:20:51 -0300

    feat(Matches)
    Implementado um meddleware para validação do requisito 25 e 26

--- app/backend/src/middlewares/validations.ts
+++ app/backend/src/middlewares/validations.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
 import { Schema } from 'joi';
 import LoginService from '../services/Login';
+import TeamService from '../services/Teams';
 import Token from '../utils/token';
 import { TUserValidate } from '../types/TUser';
+import Team from '../database/models/teams';
 
 export default class Validations {
   private _loginService = new LoginService();
+  private _teamsService = new TeamService();
   private _token = new Token();
 
   joi = (req: Request, res: Response, next: NextFunction, schemas: Schema) => {
-    console.log('Joi', req.body);
     const { error } = schemas.validate(req.body);
     if (error) {
       return res.status(400).json({ message: error.details[0].message });
@@ -32,6 +34,20 @@ export default class Validations {
       return res.status(404).json({ message: 'User not found' });
     }
     req.body.user = finduser;
+    next();
+  };
+
+  validateTeams = async (req: Request, res: Response, next: NextFunction) => {
+    const teams: Team[] = await this._teamsService.getAll();
+    if (req.body.homeTeam === req.body.awayTeam) {
+      return res.status(401)
+        .json({ message: 'It is not possible to create a match with two equal teams' });
+    }
+    const homeTeam: boolean = teams.some((team) => req.body.homeTeam === team.id);
+    const awayTeam: boolean = teams.some((team) => req.body.awayTeam === team.id);
+    if (homeTeam === false || awayTeam === false) {
+      res.status(404).json({ message: 'There is no team with such id!' });
+    }
     next();
   };
 }
