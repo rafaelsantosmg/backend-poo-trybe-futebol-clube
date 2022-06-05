5781d9bb15cc716332400f7092e80a5cbbb43f62
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-05 16:02:47 -0300

    feat(Match Service)
    implementado a função create

--- app/backend/src/services/Matches.ts
+++ app/backend/src/services/Matches.ts
@@ -1,26 +1,28 @@
+import ThrowError from '../utils/throwError';
 import Matches from '../database/models/matches';
 import Team from '../database/models/teams';
+import { TMatche } from '../types/TMatche';
 
 export default class MatcheService {
   private _matches: Matches[];
+  private _matche: Matches;
+  private _errorThrow = new ThrowError(401, 'Progress invalid');
 
   async getAll(inProgress: boolean) {
-    if (inProgress) {
-      this._matches = await Matches.findAll({
-        where: { inProgress },
-        include: [
-          { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
-          { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
-        ],
-      });
-      return this._matches;
-    }
     this._matches = await Matches.findAll({
+      where: inProgress ? { inProgress } : {},
       include: [
         { model: Team, as: 'teamHome', attributes: { exclude: ['id'] } },
         { model: Team, as: 'teamAway', attributes: { exclude: ['id'] } },
       ],
     });
     return this._matches;
+  }
+
+  async create(matche: TMatche) {
+    const { inProgress } = matche;
+    if (inProgress === false) throw this._errorThrow;
+    this._matche = await Matches.create(matche);
+    return this._matche;
   }
 }
