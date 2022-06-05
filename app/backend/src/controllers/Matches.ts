f7181891d08aa6f235d9191bff9cf6f1428682f3
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-06-05 16:02:28 -0300

    feat(Match Controller)
    implementado a função create

--- app/backend/src/controllers/Matches.ts
+++ app/backend/src/controllers/Matches.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import MatcheService from '../services/Matches';
+import ThrowError from '../utils/throwError';
 
 export default class MatchesController {
   private _matcheService = new MatcheService();
@@ -10,5 +11,16 @@ export default class MatchesController {
     this._inProgress = inProgress === 'true';
     const matches = await this._matcheService.getAll(this._inProgress);
     return res.status(200).json(matches);
+  }
+
+  async create(req: Request, res: Response) {
+    try {
+      console.log('Controller', req.body);
+      const matche = await this._matcheService.create(req.body);
+      return res.status(201).json(matche);
+    } catch (error) {
+      const { status, message } = error as ThrowError;
+      return res.status(status).json({ message });
+    }
   }
 }
